bc9e048d33538c4f09680c6d8452934790ed8506
torazuka <torazuka@gmail.com>
2013-08-09 16:28:25 +0900

    added a binding

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -32,6 +32,7 @@ namespace EssentialTools.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>();
         }
     }
 }
