9f73aa12b134e102cbe3c960da9f83b56e7460c9
torazuka <torazuka@gmail.com>
2013-08-09 18:06:08 +0900

    added a conditional binding

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -33,6 +33,7 @@ namespace EssentialTools.Infrastructure
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
             kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
+            kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>().WhenInjectedInto<LinqValueCalculator>();
         }
     }
 }
