069284d5150c1a0e7efba7a11f657d0bf19befd2
torazuka <torazuka@gmail.com>
2013-08-09 14:39:53 +0900

    added a custom dependency resolver

---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using Ninject;
+using Ninject.Parameters;
+using Ninject.Syntax;
+using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using EssentialTools.Models;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
