011dcd49a64a51c32b71c32b3fe9f4f2385d5662
torazuka <torazuka@gmail.com>
2013-08-09 17:00:39 +0900

    modified the binding to specify a value of a property

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -32,7 +32,7 @@ namespace EssentialTools.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
-            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>();
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithPropertyValue("DiscountSize", 50M);
         }
     }
 }
--- EssentialTools/Models/Discount.cs
+++ EssentialTools/Models/Discount.cs
@@ -11,9 +11,11 @@ namespace EssentialTools.Models
 
     public class DefaultDiscountHelper : IDiscountHelper
     {
+        public decimal DiscountSize { get; set; }
+
         public decimal ApplyDiscount(decimal totalParam)
         {
-            return (totalParam - (10m / 100m * totalParam));
+            return (totalParam - (DiscountSize / 100m * totalParam));
         }
     }
 }
