7cfe1356689f38d15db10359720095b5aed1103e
torazuka <torazuka@gmail.com>
2013-08-09 14:42:31 +0900

    registered the resolver

--- EssentialTools/Global.asax.cs
+++ EssentialTools/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EssentialTools.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,8 @@ namespace EssentialTools
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
