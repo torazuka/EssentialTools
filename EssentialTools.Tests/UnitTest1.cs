2bab330c080303b20ac8087e17d9e56d2ca0c416
torazuka <torazuka@gmail.com>
2013-08-12 19:10:55 +0900

    added an unit test

--- EssentialTools.Tests/UnitTest1.cs
+++ EssentialTools.Tests/UnitTest1.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
 
 namespace EssentialTools.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IDiscountHelper getTestObject()
         {
+            return new MinimumDiscountHelper();
+        }
+
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+
+            var discountedTotal = target.ApplyDiscount(total);
+
+            Assert.AreEqual(total * 0.9M, discountedTotal);
         }
     }
 }
