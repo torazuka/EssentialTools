762459fe232f9683cfc8f8ea300662ee5e29d12a
torazuka <torazuka@gmail.com>
2013-08-13 13:15:54 +0900

    added some unit tests

--- EssentialTools.Tests/UnitTest2.cs
+++ EssentialTools.Tests/UnitTest2.cs
@@ -28,5 +28,35 @@ namespace EssentialTools.Tests
 
             Assert.AreEqual(products.Sum(e => e.Price), result);
         }
+
+        private Product[] createProduct(decimal value)
+        {
+            return new[] { new Product { Price = value } };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0))).Throws<System.ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100))).Returns<decimal>(total => (total * 0.9M));
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive))).Returns<decimal>(total => total - 5);
+            var target = new LinqValueCalculator(mock.Object);
+
+            decimal FiveDollerDiscount = target.ValueProducts(createProduct(5));
+            decimal TenDollerDiscount = target.ValueProducts(createProduct(10));
+            decimal FiftyDollerDiscount = target.ValueProducts(createProduct(50));
+            decimal HundredDollerDiscount = target.ValueProducts(createProduct(100));
+            decimal FiveHundredDollerDiscount = target.ValueProducts(createProduct(500));
+
+            Assert.AreEqual(5, FiveDollerDiscount, "$5 Fail");
+            Assert.AreEqual(5, TenDollerDiscount, "$10 Fail");
+            Assert.AreEqual(45, FiftyDollerDiscount, "$50 Fail");
+            Assert.AreEqual(95, HundredDollerDiscount, "$100 Fail");
+            Assert.AreEqual(450, FiveHundredDollerDiscount, "$500 Fail");
+            target.ValueProducts(createProduct(0));
+        }
     }
 }
